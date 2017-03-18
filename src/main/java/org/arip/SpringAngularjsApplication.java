069d36ea30097a377afcaa405232b6c6e7ecd9a6
Arip Hidayat <ariphidayat@hotmail.com>
2017-03-18 21:38:45 +0700

    setup login page

--- src/main/java/org/arip/SpringAngularjsApplication.java
+++ src/main/java/org/arip/SpringAngularjsApplication.java
@@ -2,9 +2,16 @@ package org.arip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -12,6 +19,11 @@ import java.util.UUID;
 @RestController
 @SpringBootApplication
 public class SpringAngularjsApplication {
+
+    @RequestMapping(value = "user")
+    public Principal user(Principal user) {
+        return user;
+    }
 
     @RequestMapping(value = "hello")
 	public Map<String, Object> hello() {
@@ -24,4 +36,22 @@ public class SpringAngularjsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAngularjsApplication.class, args);
 	}
+
+	@Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                .httpBasic()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        }
+    }
 }
